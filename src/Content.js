faf1a53a1646b0df3f8484fa56d5c95b2d3df08c
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-02 10:04:00 +0700

    31. useEffect with DOM events-p2

--- src/Content.js
+++ src/Content.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 
 //Side effect
 
-const tabs = ["posts", "comments", "albums"];
-
 export default function Content() {
 	// 1. useEffect(callback)
 	//  - Goi callback moi khi component re-render
@@ -17,79 +15,23 @@ export default function Content() {
 	// 1. Callback luon dc goi sau khi component mounted
 	// 2. Clean up function luon dc goi truoc khi component duoc unmounted
 
-	const [title, setTitle] = useState("");
-	const [posts, setPosts] = useState([]);
-	const [type, setType] = useState("posts");
-	const [showGoToTop, setShowGoToTop] = useState(false);
+	const [width, setWidth] = useState(window.innerWidth);
 
 	useEffect(() => {
-		console.log("title change");
-		fetch(`https://jsonplaceholder.typicode.com/${type}`)
-			.then((res) => res.json())
-			.then((posts) => {
-				setPosts(posts);
-			});
-	}, [type]);
-
-	useEffect(() => {
-		const scrollHandler = () => {
-			if (window.scrollY >= 200) {
-				setShowGoToTop(true);
-			} else {
-				setShowGoToTop(false);
-			}
-
-			// setShowGoToTop(window.scrollY >= 20 0);
+		const resizeHandler = () => {
+			setWidth(window.innerWidth);
 		};
 
-		window.addEventListener("scroll", scrollHandler);
+		window.addEventListener("resize", resizeHandler);
 
-		// cleanup function, don dep bo nho khi component unmounted
 		return () => {
-			window.removeEventListener("scroll", scrollHandler);
+			window.removeEventListener("resize", resizeHandler);
 		};
 	}, []);
 
 	return (
 		<div>
-			{tabs.map((tab) => (
-				<button
-					onClick={() => setType(tab)}
-					key={tab}
-					style={
-						type === tab
-							? {
-									color: "#fff",
-									backgroundColor: "#333",
-							  }
-							: {}
-					}
-				>
-					{tab}
-				</button>
-			))}
-
-			<input
-				type="text"
-				value={title}
-				onChange={(e) => setTitle(e.target.value)}
-			/>
-			<ul>
-				{posts.map((post) => (
-					<li key={post.id}>{post.title || post.name}</li>
-				))}
-			</ul>
-			{showGoToTop && (
-				<button
-					style={{
-						position: "fixed",
-						bottom: 20,
-						right: 20,
-					}}
-				>
-					Go to top
-				</button>
-			)}
+			<h1>{width}</h1>
 		</div>
 	);
 }
