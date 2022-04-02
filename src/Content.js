ca7ca3098e12ad02ab571d85ba458bced042ec86
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-02 10:16:41 +0700

    32. useEffect with timer functions

--- src/Content.js
+++ src/Content.js
@@ -15,23 +15,22 @@ export default function Content() {
 	// 1. Callback luon dc goi sau khi component mounted
 	// 2. Clean up function luon dc goi truoc khi component duoc unmounted
 
-	const [width, setWidth] = useState(window.innerWidth);
+	// Note: Khi thay component co the bi unmounted bang bat ki truong hop nao, ma o trong component do co sd setInterval, setTimeout, async, listener event, subcribe event thi luon nho dung clean up function de tranh ro ri bo nho
+
+	const [countdown, setCountdown] = useState(180);
 
 	useEffect(() => {
-		const resizeHandler = () => {
-			setWidth(window.innerWidth);
-		};
-
-		window.addEventListener("resize", resizeHandler);
-
+		const timerId = setInterval(() => {
+			setCountdown((prev) => prev - 1);
+		}, 1000);
 		return () => {
-			window.removeEventListener("resize", resizeHandler);
+			clearInterval(timerId);
 		};
 	}, []);
 
 	return (
 		<div>
-			<h1>{width}</h1>
+			<h1>{countdown}</h1>
 		</div>
 	);
 }
