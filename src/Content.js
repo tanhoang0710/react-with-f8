e24be9d881b774772b9032c96920c41087cba5f5
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-02 10:59:34 +0700

    34.useEffect with fake Chat App

--- src/Content.js
+++ src/Content.js
@@ -2,6 +2,21 @@ import React, { useEffect, useState } from "react";
 
 //Side effect
 
+const lessons = [
+	{
+		id: 1,
+		name: "React JS",
+	},
+	{
+		id: 2,
+		name: "SPA/MPA",
+	},
+	{
+		id: 3,
+		name: "Arrow function",
+	},
+];
+
 export default function Content() {
 	// 1. useEffect(callback)
 	//  - Goi callback moi khi component re-render
@@ -19,24 +34,33 @@ export default function Content() {
 
 	// Note: Khi thay component co the bi unmounted bang bat ki truong hop nao, ma o trong component do co sd setInterval, setTimeout, async, listener event, subcribe event thi luon nho dung clean up function de tranh ro ri bo nho
 
-	const [avatar, setAvatar] = useState();
+	const [lessonId, setLessonId] = useState(1);
+
+	const commentHandler = ({ detail }) => {
+		console.log(detail);
+	};
 
 	useEffect(() => {
-		return () => {
-			avatar && URL.revokeObjectURL(avatar.preview);
-		};
-	}, [avatar]);
-
-	const previewAvatarHandler = (e) => {
-		const file = e.target.files[0];
-		file.preview = URL.createObjectURL(file);
-		setAvatar(file);
-	};
+		window.addEventListener(`lesson-${lessonId}`, commentHandler);
+		return () =>
+			window.removeEventListener(`lesson-${lessonId}`, commentHandler);
+	}, [lessonId]);
 
 	return (
 		<div>
-			<input type="file" onChange={previewAvatarHandler} />
-			{avatar && <img src={avatar.preview} alt="" width="80%" />}
+			<ul>
+				{lessons.map((lesson) => (
+					<li
+						key={lesson.id}
+						style={{
+							color: lessonId === lesson.id ? "red" : "#333",
+						}}
+						onClick={() => setLessonId(lesson.id)}
+					>
+						{lesson.name}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Fake comments
+function emitComment(id) {
+	setInterval(() => {
+		window.dispatchEvent(
+			new CustomEvent(`lesson-${id}`, {
+				detail: `Noi dung comment cua lesson ${id}`,
+			})
+		);
+	}, 2000);
+}
+
+emitComment(1);
+emitComment(2);
+emitComment(3);
+
 ReactDOM.render(
 	// <React.StrictMode>
 	<App />,
