bf4a5469139d2c681021534dd798783d5aa3d075
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-04 12:51:41 +0700

    37.React.memo HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Content from "./Content";
+// import Content from "./Content";
 
-export default function App() {
+// 1. memo() -> Higher Order Component (HOC)
+// 2. useCallback()
+
+// Hooks
+// HOC
+// Render props
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	const increase = () => {
+		setCount(count + 1);
+	};
+
 	return (
-		<div>
+		<div style={{ padding: "10px 32px" }}>
 			<Content />
+			<h1>{count}</h1>
+			<button onClick={increase}>Click me!</button>
 		</div>
 	);
 }
+
+export default App;
--- src/Content.js
+++ src/Content.js
@@ -1,45 +1,10 @@
-import { useRef, useState, useEffect } from "react";
-
-// Luu cac gia tri qua 1 tham chieu ben ngoai
-// function component
-
-export default function Content() {
-	const [count, setCount] = useState(60);
-
-	const timerId = useRef();
-	const prevCount = useRef();
-	const h1Ref = useRef();
-
-	useEffect(() => {
-		prevCount.current = count;
-	}, [count]);
-
-	useEffect(() => {
-		const rect = h1Ref.current.getBoundingClientRect();
-		console.log(rect);
-	}, []);
-
-	const handleStart = () => {
-		timerId.current = setInterval(() => {
-			setCount((prev) => prev - 1);
-		}, 1000);
-
-		console.log("Start -> ", timerId.current);
-	};
-
-	const handleStop = () => {
-		clearInterval(timerId.current);
-
-		console.log("Stop -> ", timerId.current);
-	};
-
-	console.log(count, prevCount.current);
-
+import { memo } from "react";
+function Content() {
 	return (
 		<div>
-			<h1 ref={h1Ref}>{count}</h1>
-			<button onClick={handleStart}>Start</button>
-			<button onClick={handleStop}>Stop</button>
+			<h2>Hello ae F8</h2>
 		</div>
 	);
 }
+
+export default memo(Content);
