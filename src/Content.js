775cd97a5c6ac5eaa90909be2e94c259c109f2b9
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-04 09:14:21 +0700

    useLayoutEffect hook

--- src/Content.js
+++ src/Content.js
@@ -1,66 +1,33 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
-//Side effect
+// useEffect
+// 1. Cap nhat lai state
+// 2. Cap nhat DOM (mutated)
+// 3. Render lai UI
+// 4. Goi cleanup neu deps thay doi
+// 5. Goi useEffect callback
 
-const lessons = [
-	{
-		id: 1,
-		name: "React JS",
-	},
-	{
-		id: 2,
-		name: "SPA/MPA",
-	},
-	{
-		id: 3,
-		name: "Arrow function",
-	},
-];
-
+// useLayoutEffect
+// 1. Cap nhat lai state
+// 2. Cap nhat DOM (mutated)
+// 3. Goi cleanup neu deps thay doi (sync)
+// 4. Goi useLayoutEffect callback(sync)
+// 5. Render lai UI
 export default function Content() {
-	// 1. useEffect(callback)
-	//  - Goi callback moi khi component re-render
-	//  - Goi callback sau khi component them vao dom
-	// 2. useEffect(callback, [])
-	//  - Chi goi callback 1 lan sau khi component mounted
-	// 3. useEffect(callback, [deps])
-	//  - callback se duoc goi lai moi khi deps thay doi
-	//
-	// --------------------------------
-	// 1. Callback luon dc goi sau khi component mounted
-	// 2. Clean up function luon dc goi truoc khi component duoc unmounted
+	const [count, setCount] = useState(0);
 
-	// 3. Cleanup function luon dc goi truoc khi callback dc goi(tru lan mounted)
+	useLayoutEffect(() => {
+		if (count > 3) setCount(0);
+	}, [count]);
 
-	// Note: Khi thay component co the bi unmounted bang bat ki truong hop nao, ma o trong component do co sd setInterval, setTimeout, async, listener event, subcribe event thi luon nho dung clean up function de tranh ro ri bo nho
-
-	const [lessonId, setLessonId] = useState(1);
-
-	const commentHandler = ({ detail }) => {
-		console.log(detail);
+	const handleRun = () => {
+		setCount(count + 1);
 	};
-
-	useEffect(() => {
-		window.addEventListener(`lesson-${lessonId}`, commentHandler);
-		return () =>
-			window.removeEventListener(`lesson-${lessonId}`, commentHandler);
-	}, [lessonId]);
 
 	return (
 		<div>
-			<ul>
-				{lessons.map((lesson) => (
-					<li
-						key={lesson.id}
-						style={{
-							color: lessonId === lesson.id ? "red" : "#333",
-						}}
-						onClick={() => setLessonId(lesson.id)}
-					>
-						{lesson.name}
-					</li>
-				))}
-			</ul>
+			<h1>{count}</h1>
+			<button onClick={handleRun}>Run</button>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import "./index.css";
 import App from "./App";
 
 // Fake comments
-function emitComment(id) {
-	setInterval(() => {
-		window.dispatchEvent(
-			new CustomEvent(`lesson-${id}`, {
-				detail: `Noi dung comment cua lesson ${id}`,
-			})
-		);
-	}, 2000);
-}
+// function emitComment(id) {
+// 	setInterval(() => {
+// 		window.dispatchEvent(
+// 			new CustomEvent(`lesson-${id}`, {
+// 				detail: `Noi dung comment cua lesson ${id}`,
+// 			})
+// 		);
+// 	}, 2000);
+// }
 
-emitComment(1);
-emitComment(2);
-emitComment(3);
+// emitComment(1);
+// emitComment(2);
+// emitComment(3);
 
 ReactDOM.render(
 	// <React.StrictMode>
