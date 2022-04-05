562120926cde1d3e3a62f0af7e1d4758f57a8e9a
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-05 09:29:07 +0700

    39.useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-import React, { useCallback, useState } from "react";
-import Content from "./Content";
-// import Content from "./Content";
+import React, { useMemo, useRef, useState } from "react";
 
-// 1. memo() -> Higher Order Component (HOC)
-// 2. useCallback()
+// useCallback dung khi trueyn ham hoac object xuong component con va su dung voi React.memo
 
-// Hooks
-// HOC
-// Render props
-
-// Can dung memo thi moi dung useCallback
+// useMemo dung de ko bi tinh lai logic nao do nhieu lan: vd tinh total o duoi
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [name, setName] = useState("");
+	const [price, setPrice] = useState("");
+	const [products, setProducts] = useState([]);
 
-	const increase = useCallback(() => {
-		setCount((prev) => prev + 1);
-	}, []);
+	const nameRef = useRef();
+
+	const submitHandler = () => {
+		setProducts([
+			...products,
+			{
+				name,
+				price: +price,
+			},
+		]);
+		setName("");
+		setPrice("");
+		nameRef.current.focus();
+	};
+
+	const total = useMemo(() => {
+		const result = products.reduce((res, prod) => res + prod.price, 0);
+		return result;
+	}, [products]);
 
 	return (
 		<div style={{ padding: "10px 32px" }}>
-			<Content onIncrease={increase} />
-			<h1>{count}</h1>
+			<input
+				type="text"
+				value={name}
+				placeholder="Enter name..."
+				onChange={(e) => setName(e.target.value)}
+				ref={nameRef}
+			/>
+			<br />
+			<input
+				type="text"
+				value={price}
+				placeholder="Enter price..."
+				onChange={(e) => setPrice(e.target.value)}
+			/>
+			<br />
+			<button onClick={submitHandler}>Add</button>
+			<br />
+			Total: {total}
+			<ul>
+				{products.map((product, index) => (
+					<li key={index}>
+						{product.name} - {product.price}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
