2f6b0f1c0c4712f2003d99268921e61b5df02b63
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-06 13:26:45 +0700

    41. Todo App with useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
 
 // useState
 // 1. Init state: 0
@@ -12,33 +12,107 @@ import React, { useReducer } from "react";
 
 // Init state
 
-const initState = 0;
+const initState = {
+	job: "",
+	jobs: [],
+};
 
 // Actions
-const UP_ACTION = "up";
-const DOWN_ACTION = "down";
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
+
+const setJob = (payload) => {
+	return {
+		type: SET_JOB,
+		payload,
+	};
+};
+
+const addJob = (payload) => {
+	return {
+		type: ADD_JOB,
+		payload,
+	};
+};
+
+const deleteJob = (payload) => {
+	return {
+		type: DELETE_JOB,
+		payload,
+	};
+};
 
 // Reducer
 const reducer = (state, action) => {
-	console.log("Reducer is running....");
-	switch (action) {
-		case UP_ACTION:
-			return state + 1;
-		case DOWN_ACTION:
-			return state - 1;
+	let newState;
+
+	switch (action.type) {
+		case SET_JOB:
+			newState = {
+				...state,
+				job: action.payload,
+			};
+			break;
+		case ADD_JOB:
+			newState = {
+				...state,
+				jobs: [...state.jobs, action.payload],
+			};
+			break;
+		case DELETE_JOB:
+			const newJobs = [...state.jobs];
+
+			newJobs.splice(action.payload, 1);
+
+			newState = {
+				...state,
+				jobs: newJobs,
+			};
+			break;
 		default:
 			throw new Error("Invalid action");
 	}
+
+	return newState;
 };
 
 function App() {
-	const [count, dispatch] = useReducer(reducer, initState);
+	const [state, dispatch] = useReducer(reducer, initState);
+
+	const inputRef = useRef();
+
+	const { job, jobs } = state;
+
+	const handleSubmit = () => {
+		dispatch(addJob(job));
+		dispatch(setJob(""));
+		inputRef.current.focus();
+	};
 
 	return (
 		<div style={{ padding: "0 20px" }}>
-			<h1>{count}</h1>
-			<button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
-			<button onClick={() => dispatch(UP_ACTION)}>Up</button>
+			<h3>Todo</h3>
+			<input
+				type="text"
+				placeholder="Enter todo..."
+				value={job}
+				onChange={(e) => {
+					dispatch(setJob(e.target.value));
+				}}
+				ref={inputRef}
+			/>
+			<button onClick={handleSubmit}>Add</button>
+			<ul>
+				{jobs.map((job, index) => (
+					<li key={index}>
+						{job}{" "}
+						<span onClick={() => dispatch(deleteJob(index))}>
+							&times;
+						</span>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
