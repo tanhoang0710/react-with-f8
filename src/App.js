506f019b80b06620cc66644f804a109e8e32a1ea
tanhoang0710 <tanhoang0710@gmail.com>
2022-04-10 09:00:19 +0700

    49. Sass

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Button from "./Button/Button";
 export default function App() {
 	return (
 		<div>
+			<Button />
 			<Button primary />
+			<Button primary disabled />
 		</div>
 	);
 }
--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,10 +1,11 @@
 import React from "react";
 import clsx from "clsx";
-import styles from "./Button.module.css";
+import styles from "./Button.module.scss";
 
-export default function Button({ primary }) {
+export default function Button({ primary, disabled }) {
 	const classes = clsx(styles.btn, {
 		[styles.primary]: primary,
+		[styles.disabled]: disabled,
 	});
 	return <button className={classes}>Click me</button>;
 }
