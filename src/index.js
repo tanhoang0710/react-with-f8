c9ff8d01da9756a2ac56dff7f656c5df329b0ef0
tanhoang0710 <tanhoang0710@gmail.com>
2022-05-01 09:41:20 +0700

    upgrade to v18

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 // import { StoreProvider } from "./store";
 
-ReactDOM.render(
-	// <React.StrictMode>
-	// <StoreProvider>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<Router>
 		<App />
-	</Router>,
-	// </StoreProvider>,
-	// </React.StrictMode>,
-	document.getElementById("root")
+	</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
